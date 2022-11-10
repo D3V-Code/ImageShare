6ac46431540843850832edf9336079780742b75d
D3V-Code <59558566+D3V-Code@users.noreply.github.com>
2022-11-11 01:58:28 +1100

    progressin gthrough tutorial

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>To share a photo from your phone with a friend, just press the button below</Text>
+        <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}/>
+        <Text style={styles.instructions}>
+          To share a photo from your phone with a friend, just press the button below!
+        </Text>
+        <TouchableOpacity
+          onPress={() => alert('Hello world')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>
+            Pick a photo
+          </Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +28,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width:305, 
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888', 
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
